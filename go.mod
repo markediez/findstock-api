d2b910b730febb132150bcbda682c7e0a7dc5d37
Mark Emmanuel Diez <mdiez@crunchbase.com>
2020-04-16 14:20:59 -0700

    Allow CORS

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/markediez/findstock-api
 
 go 1.12
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/gorilla/handlers v1.4.2 // indirect
+	github.com/gorilla/mux v1.7.4
+)
--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 	"log"
 	"net/http"
 	"encoding/json"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 type Post struct {
-	Stock string `json:"Stock"`
-	Store string `json:"Store"`
-	Location string `json:"Location"`
+	Id int `json:"id"`
+	Stock string `json:"stock"`
+	Store string `json:"store"`
+	Location string `json:"location"`
 }
 
 var Posts []Post
@@ -22,18 +24,26 @@ func returnAllPosts(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
-	router := mux.NewRouter().StrictSlash(true)
+	router := mux.NewRouter()
 
-	router.HandleFunc("/posts", returnAllPosts)
+	header := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"})
+	origins := handlers.AllowedOrigins([]string{"*"})
 
-	log.Fatal(http.ListenAndServe(":7777", router))
+	router.HandleFunc("/posts", returnAllPosts).Methods("GET", "OPTIONS")
+
+	log.Println("Listening to port 7777")
+	err := http.ListenAndServe(":7777", handlers.CORS(header, methods, origins)(router))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
 	Posts = []Post {
-		Post { Stock: "Toilet Paper", Store: "Sacramento Co-op", Location: "2820 R St, Sacramento, CA 95816" },
-		Post { Stock: "Toilet Paper", Store: "Whole Foods Market", Location: "4315 Arden Way, Sacramento, CA 95864" },
-		Post { Stock: "Thermometer", Store: "CVS", Location: "3338 Arden Way, Sacramento, CA 95825" },
+		Post { Id: 1, Stock: "Toilet Paper", Store: "Sacramento Co-op", Location: "2820 R St, Sacramento, CA 95816" },
+		Post { Id: 2, Stock: "Toilet Paper", Store: "Whole Foods Market", Location: "4315 Arden Way, Sacramento, CA 95864" },
+		Post { Id: 3, Stock: "Thermometer", Store: "CVS", Location: "3338 Arden Way, Sacramento, CA 95825" },
 	}
 
 	handleRequests()
