8142e24b96d9caafed217ba650294a32b1f45999
Mark Emmanuel Diez <mdiez@crunchbase.com>
2020-04-16 14:52:29 -0700

    Rename stock to item

--- main.go
+++ main.go
@@ -9,18 +9,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Post struct {
+type Stock struct {
 	Id int `json:"id"`
-	Stock string `json:"stock"`
+	Item string `json:"item"`
 	Store string `json:"store"`
 	Location string `json:"location"`
 }
 
-var Posts []Post
+var Stocks []Stock
 
-func returnAllPosts(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnAllPosts")
-	json.NewEncoder(w).Encode(Posts)
+func returnAllStocks(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllStocks")
+	json.NewEncoder(w).Encode(Stocks)
 }
 
 func handleRequests() {
@@ -30,7 +30,7 @@ func handleRequests() {
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"})
 	origins := handlers.AllowedOrigins([]string{"*"})
 
-	router.HandleFunc("/posts", returnAllPosts).Methods("GET", "OPTIONS")
+	router.HandleFunc("/stocks", returnAllStocks).Methods("GET", "OPTIONS")
 
 	log.Println("Listening to port 7777")
 	err := http.ListenAndServe(":7777", handlers.CORS(header, methods, origins)(router))
@@ -40,10 +40,10 @@ func handleRequests() {
 }
 
 func main() {
-	Posts = []Post {
-		Post { Id: 1, Stock: "Toilet Paper", Store: "Sacramento Co-op", Location: "2820 R St, Sacramento, CA 95816" },
-		Post { Id: 2, Stock: "Toilet Paper", Store: "Whole Foods Market", Location: "4315 Arden Way, Sacramento, CA 95864" },
-		Post { Id: 3, Stock: "Thermometer", Store: "CVS", Location: "3338 Arden Way, Sacramento, CA 95825" },
+	Stocks = []Stock {
+		Stock { Id: 1, Item: "Toilet Paper", Store: "Sacramento Co-op", Location: "2820 R St, Sacramento, CA 95816" },
+		Stock { Id: 2, Item: "Toilet Paper", Store: "Whole Foods Market", Location: "4315 Arden Way, Sacramento, CA 95864" },
+		Stock { Id: 3, Item: "Thermometer", Store: "CVS", Location: "3338 Arden Way, Sacramento, CA 95825" },
 	}
 
 	handleRequests()
