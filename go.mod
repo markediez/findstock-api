85b44f8741fc1a903ac959f6d46f809171f29914
Mark Emmanuel Diez <mdiez@crunchbase.com>
2020-04-16 18:20:56 -0700

    Add create new endpoint

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/markediez/findstock-api
 go 1.12
 
 require (
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/gorilla/handlers v1.4.2 // indirect
 	github.com/gorilla/mux v1.7.4
 )
--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 	"log"
 	"time"
 	"net/http"
+	"io/ioutil"
 	"encoding/json"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/google/uuid"
 )
 
 type Stock struct {
-	Id int `json:"id"`
+	Id uuid.UUID `json:"id"`
 	Item string `json:"item"`
 	Store string `json:"store"`
 	Location string `json:"location"`
@@ -25,6 +27,19 @@ func returnAllStocks(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Stocks)
 }
 
+func postNewStock(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: postNewStock")
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var stock Stock
+	json.Unmarshal(reqBody, &stock)
+	stock.Id = uuid.Must(uuid.NewRandom())
+	stock.CreatedAt = time.Now().UTC().Unix() * 1000
+	Stocks = append(Stocks, stock)
+
+	json.NewEncoder(w).Encode(stock)
+}
+
 func handleRequests() {
 	router := mux.NewRouter()
 
@@ -33,6 +48,7 @@ func handleRequests() {
 	origins := handlers.AllowedOrigins([]string{"*"})
 
 	router.HandleFunc("/stocks", returnAllStocks).Methods("GET", "OPTIONS")
+	router.HandleFunc("/stocks", postNewStock).Methods("POST")
 
 	log.Println("Listening to port 7777")
 	err := http.ListenAndServe(":7777", handlers.CORS(header, methods, origins)(router))
@@ -43,9 +59,9 @@ func handleRequests() {
 
 func main() {
 	Stocks = []Stock {
-		Stock { Id: 1, Item: "Toilet Paper", Store: "Sacramento Co-op", Location: "2820 R St, Sacramento, CA 95816", CreatedAt: time.Now().AddDate(0, 0, -1).UTC().Unix() * 1000 },
-		Stock { Id: 2, Item: "Toilet Paper", Store: "Whole Foods Market", Location: "4315 Arden Way, Sacramento, CA 95864", CreatedAt: time.Now().AddDate(0, 0, -3).UTC().Unix() * 1000 },
-		Stock { Id: 3, Item: "Thermometer", Store: "CVS", Location: "3338 Arden Way, Sacramento, CA 95825", CreatedAt: time.Now().UTC().Unix() * 1000 },
+		Stock { Id: uuid.Must(uuid.NewRandom()), Item: "Toilet Paper", Store: "Sacramento Co-op", Location: "2820 R St, Sacramento, CA 95816", CreatedAt: time.Now().AddDate(0, 0, -1).UTC().Unix() * 1000 },
+		Stock { Id: uuid.Must(uuid.NewRandom()), Item: "Toilet Paper", Store: "Whole Foods Market", Location: "4315 Arden Way, Sacramento, CA 95864", CreatedAt: time.Now().AddDate(0, 0, -3).UTC().Unix() * 1000 },
+		Stock { Id: uuid.Must(uuid.NewRandom()), Item: "Thermometer", Store: "CVS", Location: "3338 Arden Way, Sacramento, CA 95825", CreatedAt: time.Now().UTC().Unix() * 1000 },
 	}
 
 	handleRequests()
